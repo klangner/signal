4f92e8def5c202cdfec3924b250ebd848c20ca39
Krzysztof Langner <klangner@gmail.com>
2015-04-22 16:29:00 +0200

    Show wave form

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,3 +1,22 @@
 
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import numpy as np
 
-print("ok")
+
+def printFileInfo(waveFileName: str):
+    (rate, data) = wavfile.read(waveFileName)
+    print(waveFileName)
+    print("Rate: %d, length:%d" % (rate, len(data)))
+
+
+def plotWave(waveFileName: str):
+    (rate, data) = wavfile.read(waveFileName)
+    xaxis=np.linspace(0, len(data)/rate, num=len(data))
+    plt.figure(1)
+    plt.title(waveFileName)
+    plt.plot(xaxis,data)
+    plt.show()
+
+if __name__ == "__main__":
+    plotWave("../data/asa.wav")
